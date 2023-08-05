2d1e5eb9174009b4a6c5acbd506dff97a853410b
Chityanj <chityanj@gmail.com>
2023-08-05 12:17:35 +0530

    Revert "Update settings.py"
    This reverts commit 679fd603ed445c7ef3f184e43fa5e06754de2ece.

--- socialsite/settings.py
+++ socialsite/settings.py
@@ -19,8 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
