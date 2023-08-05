0c8b504b9f61b11bb294375c7a516f863d3b37be
Chityanj <chityanj@gmail.com>
2023-08-05 11:32:00 +0530

    Update settings.py

--- socialsite/settings.py
+++ socialsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reunon.onrender.com']
 AUTH_USER_MODEL = "api.User" 
 
 # Application definition
